16ca50f00b4c1365ed8e68e99f728e0d0b8135c9
Shane <shaneabh777@gmail.com>
2024-01-02 15:21:19 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// document.addEventListener('mousemove', function(event) {
+//     var windowWidth = window.innerWidth;
+//     var windowHeight = window.innerHeight;
+    
+//     var mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
+//     var mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
+    
+//     var radialGradient = 'radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, #141416 40%, rgba(0, 0, 113, 0.64) 100%, rgba(18, 18, 203, 0.64) 10%)';
+    
+//     document.body.style.background = radialGradient;
+// });
+
+var link = document.getElementById("back-to-top");
+var amountScrolled = 250;
+
+window.addEventListener('scroll', function(e) {
+    if (window.pageYOffset > amountScrolled) {
+        link.classList.add('show');
+        this.document.querySelector('header').style.opacity = 0.3;
+    } else {
+        link.className = 'back-to-top';
+        this.document.querySelector('header').style.opacity = 1;
+    }
+});
+
+// link.addEventListener('click', function(e) {
+//     e.preventDefault();
+
+//     var distance = 0 - window.pageYOffset;
+//     var increments = distance / (1000 / 16);
+
+//     function animateScroll() {
+//         window.scrollBy(0, -7);
+//         if (window.scrollY <= document.body.offsetTop) {
+//             cancelAnimationFrame(runAnimation);
+//         } else {
+//             requestAnimationFrame(animateScroll);
+//         }
+//     }
+
+//     // Start the animation
+//     var runAnimation = requestAnimationFrame(animateScroll);
+// });
