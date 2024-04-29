a40114028ffef86de9063cca9b028654be8c1180
Shane <shaneabh777@gmail.com>
2024-04-28 22:25:38 -0400

    Added new projects and modified project structure

--- js/script.js
+++ js/script.js
@@ -80,3 +80,20 @@ let tl2 = gsap.timeline({
 });
 
 tl2.fromTo(".down-arrow" ,{opacity: 0}, {opacity: 1, duration: 25});
+
+
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+document.getElementById("defaultOpen").click();
